98c1e7219d41ec2e083f036bae18c19b9b6351a2
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-05-10 09:40:52 +0200

    Removed old version

--- Bonjourr Startpage/Resources/Script.js
+++
@@ -1,6 +0,0 @@
-const show = (enabled) => document.body.className = enabled ? 'on' : 'off'
-const postMessage = msg => webkit.messageHandlers.controller.postMessage(msg)
-
-document.querySelector("button.open-preferences").addEventListener("click", () => postMessage('open-preferences'));
-document.querySelector("button.close-window").addEventListener("click", () => postMessage('close-window'));
-
